46040c8caedbddbcabfe22c28e0b47ff635ad9fc
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-01-04 10:07:44 +0100

    Optimization files added

---
+++ build.js
@@ -0,0 +1,12 @@
+({
+    // Repertoire du fichier main
+    baseUrl: "app/",
+    // Ne pas oublier de rajouter le chemin de jQuery
+    paths: {
+        "jquery": "../libs/require-jquery"
+    },
+    // Nom du fichier main
+    name: "main",
+    // Nom du fichier qui sera généré après optimisation
+    out: "main-built.js"
+})
